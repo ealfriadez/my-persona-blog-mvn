d386ee333f6dfc70f37f7876965af5d0bd9541a2
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-07-08 08:53:20 -0500

    First commit

---
+++ src/main/java/pe/edu/unfv/MyPersonaBlogMvnApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonaBlogMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonaBlogMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/unfv/MyPersonaBlogMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonaBlogMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
